6abb58644a69cfd672bb0565e19e15a1c73f3c2e
mansari2 <mansari2@illinois.edu>
2015-03-29 21:47:54 -0500

    initial empty project

---
+++ TabbedApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabbedApp
+//
+//  Created by Maazin Ansari on 3/29/15.
+//  Copyright (c) 2015 Maazin Ansari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
